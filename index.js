fcfa88fd3528c3fb0c38c298ba1bb73d04551b89
Moattar Ansari <moattaransari62@gmail.com>
2024-11-19 07:32:34 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+"use strict";
+// Data types...
+// yeh name se hi show kr rha ha k koi data ha hm uski type btain gy...
+// There are many datatypes in typescript but the commonly used ones are:...
+// 1. String : jo bhi double qoutes main ho uski datatype "string" hogi...
+// 2.number : jo bhi data number man ho woh uski data type "number" hogi...
+// 3.boolean : jo bhi data true or false main ho uski datatype "boolean" hogi...
+let AttitudeGirl = "Moattar Ansari";
+let nationality = "Pakistani";
+let region = "Allhamdulilah Muslim";
+let age = 17;
+let cricketlove = true;
+console.log(AttitudeGirl);
+console.log(nationality);
+console.log(region);
+console.log(age);
+console.log(cricketlove);
+// Assigment 1.
+// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+// let person:string = "Hello Eric,"
+// console.log(`"${person} would you like to learn some python today?"`)
---
+++ index.ts
@@ -0,0 +1,66 @@
+// Data types...
+
+// yeh name se hi show kr rha ha k koi data ha hm uski type btain gy...
+
+// There are many datatypes in typescript but the commonly used ones are:...
+
+// 1. String : jo bhi double qoutes main ho uski datatype "string" hogi...
+// 2.number : jo bhi data number man ho woh uski data type "number" hogi...
+// 3.boolean : jo bhi data true or false main ho uski datatype "boolean" hogi...
+
+    let AttitudeGirl:string = "Moattar Ansari";
+    let nationality:string = "Pakistani";
+    let region:string = "Allhamdulilah Muslim"
+    let age : number = 17;
+    let cricketlove : boolean = true;
+
+ console.log(AttitudeGirl);
+ console.log(nationality);
+ console.log(region);
+ console.log(age);
+ console.log(cricketlove);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Assigment 1.
+
+// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+// let person:string = "Hello Eric,"
+
+// console.log(`"${person} would you like to learn some python today?"`)
